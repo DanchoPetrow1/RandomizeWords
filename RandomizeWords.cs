b640480b277c94bdbf88026bce7cb2dd00642962
Yordan Petrov <danchopetrow26@gmail.com>
2025-07-15 08:37:10 +0300

    Add files via upload

---
+++ RandomizeWords.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace RandomizeWords;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] words = Console.ReadLine().Split();
+        Random random = new();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            int randomIndex = random.Next(0, words.Length);
+            Swap(i, randomIndex, words);
+        }
+        foreach (string word in words)
+        {
+            Console.WriteLine(word + " ");
+        }
+    }
+
+    static void Swap(int firstIndex, int secondIndex, string[] words)
+    {
+        string temp = words[firstIndex];
+        words[firstIndex] = words[secondIndex];
+        words[secondIndex] = temp;
+    }
+
+
+
+}
